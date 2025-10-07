00eb2de26f7848cf8f0fa412e032df8a02ed50ea
Camellia <2077767215@qq.com>
2025-10-07 22:11:58 +0800

    DAY-8 箭头函数带来this的变化，this的构造原理 ，call方法

--- js-higher/DAY-8/4.2.js
+++ js-higher/DAY-8/4.2.js
@@ -1,9 +1,14 @@
-(function () {
-    function hd() {
-        console.log("4.2 js-hd");
-    };
-    function show() {
-        console.log("4.2 js-show");
-    };
-    window.js2 = { hd, show };
-})(window);
+let lesson = {
+    site: "后盾人",
+    lists: ["js", "css", "mysql"],
+    show: function () {
+        // console.log(this.site);
+        const self = this;
+        return this.lists.map(function (value) {
+            // console.log(this);  //window
+            // console.log(self.lists);
+            return `${self.site}-${value}`;
+        }, this);  //map 的第二个参数就是指定回调时的this值
+    }
+}
+console.log(lesson.show());         
