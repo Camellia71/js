742ffae0d8da19cd70034dc79bb58a2c59ab03ab
Camellia <2077767215@qq.com>
2025-11-01 23:10:21 +0800

    DAY-10解构

---
+++ js-higher/DAY-10/函数参数的解构特性.js
@@ -0,0 +1,5 @@
+function hd(name, { sex, age }) {
+  console.log(name, sex, age);
+}
+// hd(["向军", 18]);
+hd("向军", { sex: "男", age: 18 });
---
+++ js-higher/DAY-10/多层对象的解构操作.js
@@ -0,0 +1,11 @@
+let hd = {
+  uname: "后盾人",
+  lesson: {
+    title: "javascript",
+  },
+};
+let {
+  uname,
+  lesson: { title },
+} = hd;
+console.log(uname, title);
