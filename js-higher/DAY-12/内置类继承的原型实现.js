aca470217bc1a8a07973ab4adaf223b00baf3af8
Camellia <2077767215@qq.com>
2025-11-24 22:36:17 +0800

    DAY-12完结 进入模块化开发DAY-13

--- js-higher/DAY-12/内置类继承的原型实现.js
+++ js-higher/DAY-12/内置类继承的原型实现.js
@@ -1,13 +1,39 @@
-function Arr(...args) {
-  args.forEach((item) => this.push(item));
-  this.first = function () {
+// function Arr(...args) {
+//   args.forEach((item) => this.push(item));
+//   this.first = function () {
+//     return this[0];
+//   };
+//   this.max = function () {
+//     return this.sort((a, b) => b - a)[0];
+//   };
+// }
+// Arr.prototype = Object.create(Array.prototype);
+// let hd = new Arr(1, 2, 3, 4, 5, 6, 77);
+// console.log(hd.first());
+// console.log(hd.max());
+
+class Arr extends Array {
+  constructor(...args) {
+    super(...args);
+  }
+  first() {
     return this[0];
-  };
-  this.max = function () {
+  }
+  max() {
     return this.sort((a, b) => b - a)[0];
-  };
+  }
+  add(item) {
+    this.push(item);
+  }
+  remove(value) {
+    let pos = this.findIndex((item) => item == value);
+    this.splice(pos, 1);
+  }
 }
-Arr.prototype = Object.create(Array.prototype);
 let hd = new Arr(1, 2, 3, 4, 5, 6, 77);
 console.log(hd.first());
 console.log(hd.max());
+hd.add(88);
+console.log(hd);
+hd.remove(4);
+console.log(hd);
