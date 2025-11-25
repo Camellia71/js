69fd099db591d3e37ce6a14d0697e7fe5b63f427
Camellia <2077767215@qq.com>
2025-11-25 23:07:04 +0800

    DAY-13 完结-模块化开发

---
+++ js-higher/DAY-13/houdunren.js
@@ -0,0 +1,9 @@
+import { site } from "./site.js";
+let url = "houdunren.com";
+// console.log("url.js");
+console.log(site);
+
+import { obj } from "./site.js";
+// console.log(obj);
+let count = obj.get().length;
+export { count };
